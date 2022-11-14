4ee20bca7cf48cb9745e1ddb0fb4a530af7b88d6
Sean Michael Shea <seanshea1337@gmail.com>
2022-11-13 19:15:07 -0500

    rework logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,197 +1,197 @@
-$( document ).ready(function() {
-   
-    // Create the Heros
-    var Hero = {
-        mario: {
-            name:"Mario", 
-            health: 100, 
-            attack: 20,
-            sprite: "assets/images/sprite/hero1.gif",
-        },
-        link: {
-            name:"Link", 
-            health: 100, 
-            attack: 20,
-            sprite: "assets/images/sprite/hero2.gif",
-        },
-    };
+$(document).ready(function () {
+	// Create the Heros
+	var Hero = {
+		mario: {
+			name: "Mario",
+			health: 100,
+			attack: 20,
+			sprite: "assets/images/sprite/hero1.gif",
+		},
+		link: {
+			name: "Link",
+			health: 100,
+			attack: 20,
+			sprite: "assets/images/sprite/hero2.gif",
+		},
+	};
 
-    // Creates the Villains
-    var Villain = {
-        bowser: {
-            name:"Bowser", 
-            health: 80, 
-            attack: 25,
-            sprite: "assets/images/sprite/villain2.gif",
-        },
-        moblin: {
-            name:"Moblin", 
-            health: 75, 
-            attack: 25,
-            sprite: "assets/images/sprite/villain1.gif",
-        },
-    };
+	// Creates the Villains
+	var Villain = {
+		bowser: {
+			name: "Bowser",
+			health: 80,
+			attack: 25,
+			sprite: "assets/images/sprite/villain2.gif",
+		},
+		moblin: {
+			name: "Moblin",
+			health: 75,
+			attack: 25,
+			sprite: "assets/images/sprite/villain1.gif",
+		},
+	};
 
-    // Allow you to select the hero
-    function chooseHero() {
-        $('.hero1').on('click', function() {
-            addHero(Hero.mario);
-            disableClick();
-        });
+	selectHero = () => {
+		$(".hero1").on("click", () => {
+			addHero(Hero.mario);
+			removeSelect();
+		});
 
-        $('.hero2').on('click', function() {
-            addHero(Hero.link);
-            disableClick();
-        });
-    }
-    chooseHero();
+		$(".hero2").on("click", () => {
+			addHero(Hero.link);
+			removeSelect();
+		});
+	};
 
-    //  adds hero to the front-end based on click.
-    function addHero(playerGood) {
-        //adds hero sprite
-        $('.hero-sprite-container').html('<img class="sprite" src="'+ playerGood.sprite + '">');
-        // adds hero name
-        $('.hero-name').html(playerGood.name);
-        // adds heros health
-        $('.hero-health .current-health').html(playerGood.health);
-        // adds hero attacked health
-        $('.hero-health .attacked-health').html(playerGood.health);
-        
-        // adds the randon villain
-        chooseVillain(randomVillain());
-    }
+	selectHero();
 
-    // disables the hero selection click and hides selection 
-    function disableClick() {
-        $('.hero-picker').css('display', 'none');
-        $('.hero-picker img').css('pointer-events', 'none');
-        $('.health').css('display', 'block');
-        $('.player-info').css('display', 'block');
+	addHero = (hero) => {
+		let heroName = hero.name;
+		let heroHealth = hero.health;
+		let heroSprite = hero.sprite;
+		let heroOrginalAttackPoint = hero.attack;
 
-    }
+		$(".hero-name").html(heroName);
+		$(".hero").attr("attack", heroOrginalAttackPoint);
+		$(".hero-health .full-health").html(heroHealth);
+		$(".hero-health .current-health").html(heroHealth);
+		$(".hero-sprite-container").append(
+			`<img class='sprite' src='${heroSprite}'/>`
+		);
 
-    //  chooses a random Villain
-    function randomVillain() {
-        var listKeys = Object.keys(Villain);
-        var randomIndex = Math.floor(Math.random() * listKeys.length);
-        var randomObject = Villain[listKeys[randomIndex]];
-        return randomObject;
-    }
+		addVillain(randomVillain());
+	};
 
-    function chooseVillain(playerBad) {
-        $('.villain-sprite-container').html('<img class="sprite" src="'+ playerBad.sprite + '">');
-        $('.villain-name').html(playerBad.name);
-        $('.villain-health .current-health').html(playerBad.health);
-        $('.villain-health .attacked-health').html(playerBad.health);
-    }
+	//  Selects a random Villain
+	randomVillain = () => {
+		var listKeys = Object.keys(Villain);
+		var randomIndex = Math.floor(Math.random() * listKeys.length);
+		var randomObject = Villain[listKeys[randomIndex]];
+		return randomObject;
+	};
 
-    function playerAttack() {
+	addVillain = (villain) => {
+		let villainName = villain.name;
+		let villainHealth = villain.health;
+		let villainSprite = villain.sprite;
 
-        function heroAttack() {
-            if ( $('.hero-name').html() == "Mario") {
-                var heroAttack = Hero.mario.attack;
-            } else {
-                var heroAttack = Hero.link.attack;
-            }
-    
-             heroAttack = Math.floor((Math.random() * heroAttack) + 1);
+		$(".villain-name").html(villainName);
+		$(".villain-health .full-health").html(villainHealth);
+		$(".villain-health .current-health").html(villainHealth);
+		$(".villain-sprite-container").append(
+			`<img class='sprite' src='${villainSprite}'/>`
+		);
+		$(".villain-sprite-container").addClass(villainName);
+	};
 
-                       
-            if ( $('.villain-name').html() == "Bowser"  && Villain.bowser.health  > 0) {
-                Villain.bowser.health -= heroAttack;
-                screenContent();
-            } 
-            
-            if ($('.villain-name').html() == "Bowser" && Villain.bowser.health  < 0) {
-                $('.villain-health .attacked-health').html('0');
-                $('.villain-sprite-container').css('display', 'none');
-            }
-            
-    
-            if ( $('.villain-name').html() == "Moblin"  && Villain.moblin.health  > 0) {
-                Villain.moblin.health -= heroAttack;
-                screenContent();
-            } 
-            
-            if ($('.villain-name').html() == "Moblin" && Villain.moblin.health  < 0) {
-                $('.villain-health .attacked-health').html('0');
-                $('.villain-sprite-container').css('display', 'none');
-            }
+	removeSelect = () => {
+		$(".hero-picker").css("display", "none");
+		$(".health").css("display", "block");
+		$(".player-info").css("display", "block");
+	};
 
-            $('.villain-sprite-container').toggleClass('attack-effect');
+	battle = () => {
+		let heroName = $(".hero-name").html().toLowerCase();
+		let villainName = $(".villain-name").html().toLowerCase();
+		let heroStats = Hero[heroName];
+		let villainStats = Villain[villainName];
 
-            $('.attack-button').css('pointer-events', 'none').toggleClass('disabled');
-        }
+		attack = (heroStats, villainStats) => {
+			checkHealthVillain();
+			let = heroAttackPoints = heroStats.attack;
+			let = villainHealthPoints = $(
+				".villain-health .current-health"
+			).html();
 
-        heroAttack();
+			heroAttackPoints = Math.floor(Math.random() * heroAttackPoints + 1);
+			let criticalAttackPoints = criticalAttack();
 
-        setTimeout( () => {
-            function villainAttack(){
+			if (criticalAttackPoints > 75) {
+				heroAttackPoints += 25;
+			}
 
-                if ( $('.villain-name').html() == "Bowser") {
-                    var villainAttack = Villain.bowser.attack;
-                } else {
-                    var villainAttack = Villain.moblin.attack;
-                }
-        
-                villainAttack = Math.floor((Math.random() * villainAttack) + 1);
-                
-                if ( $('.hero-name').html() == "Link"  && Hero.Link.health > 0) {
-                    Hero.link.health -= villianAttack;
-                    screenContent();
-                } 
-                
-                if ($('.hero-name').html() == "Link" && Hero.link.health < 0) {
-                    $('.hero-health .attacked-health').html('0');
-                    $('.hero-sprite-container').css('display', 'none');
-                }
-                
-        
-                if ( $('.hero-name').html() == "Mario"  && Hero.mario.health > 0) {
-                    Hero.mario.health -= villainAttack;
-                    screenContent();
-                } 
-                
-                if ($('.hero-name').html() == "Mario" && Hero.mario.health < 0) {
-                    $('.hero-health .attacked-health').html('0');
-                    $('.hero-sprite-container').css('display', 'none');
-                }
-            }
-            $('.villain-sprite-container').toggleClass('attack-effect');
-            $('.attack-button').css('pointer-events', 'unset').toggleClass('disabled');
-            villainAttack();
-            $('.hero-sprite-container').toggleClass('attack-effect');
-        }, 3000);
+			villainHealthPoints -= heroAttackPoints;
+			$(".villain-health .current-health").html(villainHealthPoints);
+			$(".villain-health .health-status").css(
+				"width",
+				villainHealthPoints + "%"
+			);
 
-     
+			$(".hero .sprite").toggleClass("good-attack");
+			$(".villain .sprite").removeClass("bad-attack");
+			$(".attack-button")
+				.css("pointer-events", "none")
+				.toggleClass("disabled");
+		};
 
-    }
-  
+		deffend = (heroStats, villainStats) => {
+			checkHealthHero();
+			setTimeout(() => {
+				$(".hero .sprite").toggleClass("good-attack");
+				$(".villain .sprite").toggleClass("bad-attack");
+				let = villainAttackPoints = villainStats.attack;
+				let = heroHealthPoints = $(
+					".hero-health .current-health"
+				).html();
+				heroAttackPoints = Math.floor(
+					Math.random() * heroAttackPoints + 1
+				);
 
-function screenContent() {
-    if ( $('.villain-name').html() == "Bowser") {
-        $('.villain-health .attacked-health').html(Villain.bowser.health);
-    }
+				heroHealthPoints -= villainAttackPoints;
 
-    if ( $('.villain-name').html() == "Moblin") {
-        $('.villain-health .attacked-health').html(Villain.moblin.health);
-    }
+				$(".hero-health .current-health").html(heroHealthPoints);
 
-    if ( $('.hero-name').html() == "Mario") {
-        $('.hero-health .attacked-health').html(Hero.mario.health);
-    }
+				$(".hero-health .health-status").css(
+					"width",
+					heroHealthPoints + "%"
+				);
 
-    if ( $('.hero-name').html() == "Link") {
-        $('.hero-health .attacked-health').html(Hero.link.health );
-    }
-}
+				$(".attack-button")
+					.css("pointer-events", "unset")
+					.toggleClass("disabled");
+			}, 3000);
+		};
 
-    $('.attack-button').on('click', function(){
-        playerAttack();
-        $('.hero-sprite-container.attack-effect').removeClass('attack-effect');
-    });
+		attack(heroStats, villainStats);
+		deffend(heroStats, villainStats);
+	};
 
+	function criticalAttack() {
+		let criticalChance = Math.floor(Math.random() * 100) + 1;
+		return criticalChance;
+	}
+
+	checkHealthHero = () => {
+		let = heroHealthPoints = $(".hero-health .current-health").html();
+
+		console.log("heroHealthPoints", heroHealthPoints);
+
+		if (heroHealthPoints <= 0) {
+			console.log("you lose");
+			$(".hero .sprite").html("You Lose");
+			$(".attack-button")
+				.css("pointer-events", "none")
+				.toggleClass("disabled");
+
+			return;
+		}
+	};
+
+	checkHealthVillain = () => {
+		let = villainHealthPoints = $(".villain-health .current-health").html();
+
+		console.log("villainHealthPoints", villainHealthPoints);
+		if (villainHealthPoints <= 0) {
+			console.log("you win");
+			$(".villain .sprite").html("Winner");
+			$(".attack-button")
+				.css("pointer-events", "none")
+				.toggleClass("disabled");
+			return;
+		}
+	};
+
+	$(".attack-button").on("click", () => {
+		battle();
+	});
 });
-
-
-
